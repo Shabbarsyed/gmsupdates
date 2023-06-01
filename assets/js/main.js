0a418bf6ec344f4eeea740ca549795aed99bdf00
Shabbar Ali <72815449+Shabbarsyed@users.noreply.github.com>
2023-06-01 16:19:27 +0500

    nnn

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,3 +192,25 @@ function animateValue(obj, start, end, duration) {
   
   const obj3 = document.getElementById("weblaunched");
   animateValue(obj3, 0, 29, 5000);
+
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+  var toggler = document.getElementsByClassName("clickit");
+  var i;
+
+  for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function () {
+      this.parentElement.querySelector(".nested").classList.toggle("active");
+      this.classList.toggle("caret-down");
+    });
+  }
